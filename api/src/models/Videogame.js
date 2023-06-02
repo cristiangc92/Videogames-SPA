977869c576cbf78f35d0b792133db6c21e232a5e
unknown <cristiangc92@gmail.com>
2023-06-02 00:32:20 -0300

    Modelo Videogames completado

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };

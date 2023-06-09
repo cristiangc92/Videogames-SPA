0b43ca2dfc9aa47f18c9a494f779e811f2bb2e28
unknown <cristiangc92@gmail.com>
2023-06-09 01:35:47 -0300

    Realizando deploy.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,6 @@ import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 

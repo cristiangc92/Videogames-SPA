23aeb0fe52016af77e4c0e5a3523d468f251e393
unknown <cristiangc92@gmail.com>
2023-06-09 03:18:54 -0300

    Realizando el deploy.

--- client/src/components/VideogameCreate.jsx
+++ client/src/components/VideogameCreate.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postVideogame, getGenres, getVideogames } from "../actions";
+import { Link } from "react-router-dom";
 import "./VideogameCreate.css";
 import "bootstrap/dist/css/bootstrap.css";
 
